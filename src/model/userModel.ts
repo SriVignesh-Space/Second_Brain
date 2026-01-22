f138e6699316cb25f0cdc53a774421ff1672086b
SriVignesh-Space <srivignesh1149@gmail.com>
2026-01-22 22:45:31 +0530

    feat: authentication backend added

---
+++ src/model/userModel.ts
@@ -0,0 +1,24 @@
+import * as z from "zod"
+import mongoose, { Schema } from "mongoose";
+
+
+// zod validation and types
+const userObject =  z.object({
+        username: z.string(),
+        password: z.string("min 3 character required").min(3).regex(/^[A-Z]/),
+        email : z.email(),
+    })
+
+export default userObject;
+
+export type userType = z.infer<typeof userObject>
+
+
+// mongoose user 
+const userSchema = new Schema({
+    username : String,
+    email : String,
+    password : String
+})
+
+export const userModel = mongoose.model('User', userSchema);
