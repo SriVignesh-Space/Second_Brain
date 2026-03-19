1905686d15c284d0fd90f8d23b9b86b628865f36
SriVignesh-Space <srivignesh1149@gmail.com>
2026-03-19 22:56:20 +0530

    Two repo merged

--- backend-repo/src/router/userRouter.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import { signin, signup } from '../controller/userController.js'
-const userRouter = express.Router()
-
-userRouter.post("/signup", signup)
-userRouter.post("/signin", signin)
-
-export default userRouter
--- backend-repo/src/util/random.ts
+++
@@ -1,6 +0,0 @@
-import crypto from 'crypto'
-
-export default function getCode(){
-    const code : String = crypto.randomBytes(4).toString('hex');
-    return code;
-}
